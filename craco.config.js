299fafdd698ead6e531f7ed0e8ce2e66eed9f5d3
ShovelTime <gousaid67@gmail.com>
2024-10-17 15:15:48 +1100

    craco saves the day

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,23 @@
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
 module.exports = {
     webpack: {
+      plugins: {
+	add: [ new WasmPackPlugin({
+            crateDirectory: './src/sql-static-analyzer/',
+
+            // Check https://rustwasm.github.io/wasm-pack/book/commands/build.html for
+            // the available set of arguments.
+            //
+            // Optional space delimited arguments to appear before the wasm-pack
+            // command. Default arguments are `--verbose`.
+            args: '--log-level warn',
+            // Default arguments are `--typescript --target browser --mode normal`.
+            extraArgs: '--no-typescript',
+
+        })],
+
+      },
       configure: (webpackConfig) => {
         // Needed for sql.js
         webpackConfig.resolve.fallback = {
